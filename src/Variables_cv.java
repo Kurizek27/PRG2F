6fcf3b9d0912d539078ee6bdb7a968c489eb3556
Alois Volenec <alois.volenec@infis.cz>
2024-09-19 15:00:18 +0200

    NAHaha

---
+++ src/Variables_cv.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Variables_cv {
+    public static void main(String[] args) {
+        // připomínka z minulé hodiny
+
+        // Rozdělte datové typy k proměnnám
+        // String, boolean, char, int, double
+
+
+//        type vek = 0;                         // celá čísla v rozsahu cca -2 miliardy až ~2 miliardy
+//        type PI = 3.14159d;                   // reálná čísla (s desetinou čárkou)
+//        type isTrue = true;                   // pravdivostní hodnota true/false (ano/ne)
+//        type bigA = 'A';                      // jeden znak, píše se do ''
+//        type name = "Jmeno";                  // textový řetězec (znak/slovo/text), píše se do "";
+
+        /**
+         * CV 1
+         * Uložte 2 libovolná čísla do 2 proměnných
+         * Poté je:
+         * Vypište, Přehoďte, Vypište,
+         */
+
+
+        /**
+         * CV 2
+         * Napište jednoduchou kalkulačku, která bude umět +, -, *, / nad dvěma čísly z množiny R (Reálných čísel).
+         * Výsledky vypište na konzoli
+         */
+
+        /**
+         * CV 3
+         * Uložte do proměnné pravdivostního typu, zda součet 3 libovolných malých písmen je větší než 300.
+         * Výsledek vypište na konzoli
+         */
+
+        /**
+         * CV 4
+         * Zeptejte se uživatele na jeho, jméno, příjmení, PSČ a rodné číslo
+         * Údaje pak uživateli vypište
+         */
+    }
+}
