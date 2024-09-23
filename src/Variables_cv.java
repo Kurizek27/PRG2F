df5aa8bf3255de8db53b9343a6a3eb8dc1ddec4c
alois.volenec <alois.volenec@infis.cz>
2024-09-23 13:14:01 +0200

    xvgml

--- src/Variables_cv.java
+++ src/Variables_cv.java
@@ -38,6 +38,29 @@ public class Variables_cv {
          */
 
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Zadej Číslo 1");
+            double cislo1 = scan.nextDouble();
+        System.out.println("Zadej operaci");
+            char operace = scan.next().charAt(0);
+        System.out.println("Zadej Číslo 2");
+            double cislo2 = scan.nextDouble();
+        switch (operace) {
+            case '+':
+                System.out.println(cislo1+cislo2);
+                break;
+            case '-':
+                System.out.println(cislo1-cislo2);
+                break;
+            case '*':
+                System.out.println(cislo1*cislo2);
+                break;
+            case '/':
+                System.out.println(cislo1/cislo2);
+                break;
+
+        }
+
 
         /**
          * CV 3
