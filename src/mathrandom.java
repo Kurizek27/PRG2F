3865c88f7adaa02cfe744581056b76959fb64b4b
Alois Volenec <alois.volenec@infis.cz>
2024-09-26 15:24:50 +0200

    26.9

--- src/mathrandom.java
+++ src/mathrandom.java
@@ -1,7 +1,9 @@
 import java.util.Random;
+import java.util.Scanner;
+
 public class mathrandom {
     public static void main(String[] args) {
-        int max = 50;
+  /*    int max = 50;
         int min = 12;
         Random rng = new Random();
         rng.setSeed(69);
@@ -9,5 +11,13 @@ public class mathrandom {
         for (int i = 0; i < 10; i++) {
             System.out.println(rng.nextInt(15, 50));
         }
+        */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Zadej nejmenší číslo");
+        int x = sc.nextInt();
+        System.out.println("Zadej největší číslo");
+        int y = sc.nextInt();
+        Random rng = new Random();
+        System.out.println(rng.nextInt(x, y));
     }
 }
