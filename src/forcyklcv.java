2cd6fdf25d0eca2e701bb87d4628045f4181939b
Alois Volenec <alois.volenec@infis.cz>
2024-11-14 15:20:33 +0100

    idks

--- src/forcyklcv.java
+++ src/forcyklcv.java
@@ -1,5 +1,6 @@
 import com.sun.nio.sctp.PeerAddressChangeNotification;
 
+import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -38,21 +39,34 @@ public class forcyklcv {
 //        }
 //        System.out.println("Procenta:"+(pocetSmrti*100)/pocetHodu);
 
-          int pocetHodu = 1_000_000;
-          int hod = -1;
-          int hlava = 0;
-          int orel = 0;
-        for (int i = 1; i < pocetHodu; i++) {
-            hod = rng.nextInt(1,3);
-            if (hod == 1){
-                hlava++;
-            }
-            else {
-                orel++;
-            }
+//          int pocetHodu = 1_000_000;
+//          int hod = -1;
+//          int hlava = 0;
+//          int orel = 0;
+//        for (int i = 1; i < pocetHodu; i++) {
+//            hod = rng.nextInt(1,3);
+//            if (hod == 1){
+//                hlava++;
+//            }
+//            else {
+//                orel++;
+//            }
+//
+//        }
+//        System.out.println("Hlava:"+(hlava*100)/pocetHodu);
+//        System.out.println("Orel:"+(orel*100)/pocetHodu);
 
+        int radky = 4;
+        int sloupce = 3;
+        int x = 1;
+        for (int i = 1; i <= radky ; i++) {
+            for (int j = 1; j <= sloupce ; j++) {
+                System.out.print("\t"+x);
+//                \t je tab \n je novy radek
+                x++;
+            }
+            System.out.println();
         }
-        System.out.println("Hlava:"+(hlava*100)/pocetHodu);
-        System.out.println("Orel:"+(orel*100)/pocetHodu);
+
     }
 }
