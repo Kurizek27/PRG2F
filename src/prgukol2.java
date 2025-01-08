ac7bcf4c4695df33d9820a9018884db042215788
pohodovejr <alois.volenec@infis.cz>
2025-01-08 14:56:41 +0100

    PC doma

--- src/prgukol2.java
+++ src/prgukol2.java
@@ -2,15 +2,18 @@ import java.util.Scanner;
 
 public class prgukol2 {
     static void validatePassword(String password){
+        boolean hasUppercase = password.matches(".*[A-Z].*");
         if (password.length() < 8){
             System.out.println("Heslo nesplňuje jednu z podmínek: \"délka\"");
         } else if (password.contains("heslo")) {
             System.out.println("Heslo nesplňuje jednu z podmínek: \"obsahuje zakázané slovo\"");
+        } else if (!(hasUppercase)) {
+            System.out.println("Heslo nesplňuje jednu z podmínek: \"Heslo neobshahuje alespoň jedno velké písmeno\"");
         } else System.out.println("Heslo je v pořádku");
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Create strong password");
+        System.out.println("Vytvořte si heslo");
         String password = sc.nextLine();
         validatePassword(password);
     }
